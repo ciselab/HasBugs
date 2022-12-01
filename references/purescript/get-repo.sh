026d21e2aa2ab247b55dff1bbc2d9bb75b5e5560
lapplislazuli <Leonhard.Applis@Protonmail.com>
2022-12-01 11:51:26 +0100

    Added PureScript and One Bug for it

---
+++ references/purescript/get-repo.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Blatantly stolen one-liner to get the directory that this script is located in,
+# rather than the location the script is called from.
+repo_dir=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
+
+# Get the (current) display name from the repo directory name.
+display_name=$(basename $repo_dir)
+
+# Move into repo directory for the relative link later
+cd $repo_dir
+
+# Call the get-repo.sh script from this location
+../../../tools/get-repo.sh "$display_name"
---
+++ references/purescript/notes.md
@@ -0,0 +1,5 @@
+# Notes on Purescript
+
+One build on my Macbook: ~11 minutes
+
+One `run-tested` takes ~1050s (60s deps, 990s build + test)
